bc704dbf8a9aaf6ca05473080e6297d0359284ba
Michael Clemens <michael.clemens@outlook.com>
2018-05-29 15:37:15 -0500

    Fixed up Code Generation Portion and Markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,53 @@ export class AppComponent {
   numberOfRows: number[] = [1,2,3,4,5,6,7,8];
   boxBackground: any[][] = [[],[],[],[],[],[],[],[]];
   mousePressed: boolean = false;
-  color: any;
+  color: any = '';
+  code: any = '';
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     for(let i = 0; i<8; i++) {
       for(let j = 0; j<16; j++) {
         this.boxBackground[i][j]='241,241,241';
       }
     }
+    this.code=`int i = 26;
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    for(int i=6; i>=0; i++) {
+      string.print("Something");
+    }
+    `;
   }
 
   boxClicked(row, column) {
@@ -35,10 +75,12 @@ export class AppComponent {
   }
 
   boxEntered(row,column) {
-    let modifiedString = this.color.slice(5);
-    modifiedString = modifiedString.split(",");
-    if(this.mousePressed) {
-      this.boxBackground[row][column] = modifiedString[0] + ',' + modifiedString[1] + ',' + modifiedString[2];
+    if(this.color!='') {
+      let modifiedString = this.color.slice(5);
+      modifiedString = modifiedString.split(",");
+      if(this.mousePressed) {
+        this.boxBackground[row][column] = modifiedString[0] + ',' + modifiedString[1] + ',' + modifiedString[2];
+      }
     }
   }
 
@@ -49,4 +91,40 @@ export class AppComponent {
       }
     }
   }
+
+  showCodeGeneration(): void {
+    let dialogRef = this.dialog.open(CodeGeneration, {
+      width: '500px',
+      height: '500px',
+      data: { yes:"yes",no:"no",code:this.code}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+}
+
+
+@Component({
+  selector: 'code-generation',
+  templateUrl: 'code-generation.html',
+  styleUrls: ['./code-generation.css']
+})
+export class CodeGeneration {
+
+  copyClicked: any;
+
+  constructor(
+    public dialogRef: MatDialogRef<CodeGeneration>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+   onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  changeCopy() {
+    this.copyClicked = 'true';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ColorPickerModule } from 'ngx-color-picker';
-import { AppComponent } from './app.component';
+import { AppComponent, CodeGeneration } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MarkdownModule } from 'ngx-markdown';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeGeneration  
   ],
   imports: [
     BrowserModule,
-    ColorPickerModule
+    ColorPickerModule,
+    BrowserAnimationsModule,
+    MarkdownModule.forRoot(),
+    MatDialogModule,
+    ClipboardModule
   ],
+  entryComponents: [CodeGeneration],
   providers: [],
   bootstrap: [AppComponent]
 })
